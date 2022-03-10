a52099690839cfc1c09b57aec95c668e9836612a
Fernando Soares <fernandosoares.dev@gmail.com>
2022-03-10 15:05:06 -0300

    Final Final

--- partials/program.php
+++ partials/program.php
@@ -15,27 +15,27 @@
         <div class="row">
             <div class="col-12 col-md-6 modulo-1 d-md-flex align-items-start flex-column justify-content-center order-one">
                 <!-- <h2>Liderança</h2> -->
-                <p class="info">Horários das aulas:<br />8h30 às 10h:</p>
+                <p class="info">Horários das aulas:<br />8h30 às 10h</p>
                 <p class="date">Início do curso:<br />4 de abril</p>
             </div>
             <div class="col-12 col-md-6 modulo-info order-two">
 
-                <p class="titulo">04/04 - Personal Branding</p>
+                <p class="titulo">04/04 - PERSONAL BRANDING</p>
                 <p class="descricao">Construindo e fortalecendo sua marca pessoal</p>
 
-                <p class="titulo">05/04 – Eneagrama</p>
+                <p class="titulo">05/04 – ENEAGRAMA</p>
                 <p class="descricao">O poder do autoconhecimento</p>
 
-                <p class="titulo">07/04 - Design your life</p>
+                <p class="titulo">07/04 - DESIGN YOUR LIFE</p>
                 <p class="descricao">Pensando a carreira dentro da sua vida</p>
 
-                <p class="titulo">11/04 - Jornada da C-Level</p>
+                <p class="titulo">11/04 - JORDANA DA C-LEVEL</p>
                 <p class="descricao">Caminhos de uma jornada desafiadora e de oportunidades</p>
 
-                <p class="titulo">12/04 - Protagonismo: você na liderança da sua vida</p>
+                <p class="titulo">12/04 - PROTAGONISMO: VOCÊ NA LIDERANÇA DA SUA VIDA</p>
                 <p class="descricao">Ampliando a autoconsciência</p>
 
-                <p class="titulo">14/04 - Liderança do Futuro</p>
+                <p class="titulo">14/04 - LIDERANÇA DO FUTURO</p>
                 <p class="descricao">A pluralidade do papel das novas lideranças</p>
 
             </div>
