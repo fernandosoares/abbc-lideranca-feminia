fe96196338898f0c17d8ce5c7d5b924243c9cee6
Fernando Soares <fernandosoares.dev@gmail.com>
2022-03-14 10:34:58 -0300

    Ultimas Alteraçoes

--- partials/partners.php
+++ partials/partners.php
@@ -7,7 +7,7 @@
             </div>
             <div class="col-12 col-md-6">
                 <img src="<?=get_template_directory_uri()?>/assets/img/FINASHE.png" alt="" />
-                <p>Plataforma para quem busca se conectar com a equidade de gênero de uma forma inspiradora.  A Fin4She acredita que a capacitação é o caminho para atingirmos os patamares desejados. Quando as mulheres assumem cargos de liderança é muito importante que se sintam preparadas e conectadas com este papel, pois como líderes estão constantemente influenciando, impactando e inspirando pessoas.</p>
+                <p class="texto-parceiro">Plataforma para quem busca se conectar com a equidade de gênero de uma forma inspiradora.  A Fin4She acredita que a capacitação é o caminho para atingirmos os patamares desejados. Quando as mulheres assumem cargos de liderança é muito importante que se sintam preparadas e conectadas com este papel, pois, como líderes estão constantemente influenciando, impactando e inspirando pessoas.</p>
             </div>
         </div>
     </div>
